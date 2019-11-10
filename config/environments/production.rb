2c510a3eab0570239a540d3022c089ad0b14a4a5
LIKHITH BALYAM <likhith@apexft.com>
2019-11-10 22:19:44 +0530

    try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,15 +33,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'sitepoint-devise.herokuapp.com' }
 
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'likith.naga@yopmail.com',
+    authentication:       'plain',
+    enable_starttls_auto: true }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
